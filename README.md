233423c293bf79aec69c212d202c4bae321e84fc
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2024-06-22 17:44:14 +0530

    update readme and and add logout in mobile drawer

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# adaan-digital
+# Adaan Digital User-Profile Aplication
+
+### API doc link: https://adaan-digital.onrender.com/docs/
+### Application : https://adaan-digital.vercel.app/login
+
+This project is a full stack implementation for an user to onBoard, view and update their profile.Backend includes User authentication with jwt token.The backend is built using Node.js, Express, and MongoDB, with API documentation provided via Swagger. Frontend built with React and Chakra-UI.
+
+## Installation for backend
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/Nitesh-Samaniya/adaan-digital.git
+    ```
+
+2. Install dependencies:
+    ```bash
+    cd server
+    npm install
+    ```
+
+3. Create a `.env` file in the root directory and add your environment variables:
+    ```plaintext
+    PORT=8080
+    MONGO_URI=your_mongodb_connection_string
+    JWT_SECRET=your_jwt_secret
+    ```
+
+
+
+- **MongoDB**: This project uses MongoDB as the database. Ensure you have a MongoDB instance running and provide the connection string in the `.env` file.
+- **JWT**: Used for user authentication. Add your JWT secret in the `.env` file.
+
+## Usage
+
+## API Documentation
+
+Swagger is used to document the API, uncomment the code in swagger.js and comment the deploy url and https.
+
+1. To autogenerate swagger doc
+    ```bash
+    node swagger.js
+    ```
+
+2. Start the server:
+    ```bash
+    npm run dev
+    ```
+
+3. The server will be running at `http://localhost:8080`.
+
+## To run frontend locally
+
+    1. Install dependencies:
+    ```bash
+    cd client
+    npm install
+    ```
+
+    2. npm run start
+
+
+## Author
+- [@Nitesh-Samaniya](https://github.com/Nitesh-Samaniya)
--- client/src/component/navbar/Drawer.jsx
+++ client/src/component/navbar/Drawer.jsx
@@ -6,16 +6,25 @@ import { useDisclosure ,DrawerBody,
     DrawerCloseButton,
     Text,
     Drawer,
-    Button
+    Button,
+    Box
 } from '@chakra-ui/react'
-import React from 'react'
+import React, { useContext } from 'react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import { Link } from "react-router-dom";
+import { AuthContext } from '../../context/AuthContext';
 
 
 const Drawerr = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+    const {isToken, setIsToken} = useContext(AuthContext);
     const btnRef = React.useRef()
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('adaanDigitalUserToken');
+    setIsToken(''); 
+  };
   
     
   return (
@@ -34,11 +43,18 @@ const Drawerr = () => {
         <DrawerHeader bg={'#010026'} color={'#fff'}>Adaan Digital</DrawerHeader>
 
         <DrawerBody bg="#0c111b" color={'#fff'}>
-          <Text cursor={'pointer'} fontSize={'2xl'} fontFamily={'cursive'} borderBottom={'1px solid gray'} mb={3}>
-            <Button size='sm' mb={2} mt={1}><Link to={'/login'}>Sign In</Link></Button>
-          </Text>
-          <Text cursor={'pointer'} fontSize={'2xl'} fontFamily={'cursive'} borderBottom={'1px solid gray'} mb={3}><Link to={'/'}>Profile</Link></Text>
-          
+          <Box>
+          <Text borderBottom={'1px solid gray'} mb={3} cursor="pointer" fontSize="2xl" fontFamily="cursive">
+                <Link to="/">Profile</Link>
+              </Text>
+              
+              {isToken ? (
+                <Button borderBottom={'1px solid gray'} mb={3} size="sm" onClick={handleLogout}>Log out</Button>
+              ) : (
+                <Button borderBottom={'1px solid gray'} mb={3} size="sm"><Link to="/login">Sign In</Link></Button>
+              )}
+              
+            </Box>
         </DrawerBody>
 
         <DrawerFooter h={'60px'}>
