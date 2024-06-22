026a48c76ae460e124c9e0b79dff8ac0ebc43609
Nitesh-Samaiya <nitinsamaniya97@gmail.com>
2024-06-22 12:30:25 +0530

    add get req to push server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
 import { Box } from "@chakra-ui/react";
 import Navbar from "./component/navbar/Navbar";
 import AllRoutes from "./AllRoutes";
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  
+  async function makeServerStart(){
+    try {
+      const resp = await axios.get('https://adaan-digital.onrender.com/')
+      console.log(resp.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    makeServerStart();
+  },[])
+
   return (
     <Box>
       <Navbar />
